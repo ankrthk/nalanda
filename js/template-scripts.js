b81c0951139ba6e61d0accf2aca85cf80b097820
ankrthk1 <75695687+ankrthk1@users.noreply.github.com>
2020-12-08 16:35:32 -0300

    Essa Agro

---
+++ js/template-scripts.js
@@ -0,0 +1,70 @@
+jQuery(document).ready(function($) {      
+  // Owl Carousel  
+  var owl = $('.carousel-default');
+  owl.owlCarousel({
+    nav: true,
+    dots: true,
+    items: 1,
+    loop: true,
+    navText: ["&#xf007","&#xf006"],
+    autoplay: true,
+    autoplayTimeout: 4000
+  });
+  var owl = $('.carousel-fade-transition');
+  owl.owlCarousel({
+    nav: true,
+    dots: true,
+    items: 1,
+    loop: true,
+    navText: ["&#xf007","&#xf006"],
+    autoplay: true,
+    animateOut: 'fadeOut',
+    autoplayTimeout: 4000
+  });  
+});
+
+window.addEventListener("DOMContentLoaded", function() {
+
+  // get the form elements defined in your form HTML above
+  
+  var form = document.getElementById("my-form");
+  var button = document.getElementById("my-form-button");
+  var status = document.getElementById("my-form-status");
+
+  // Success and Error functions for after the form is submitted
+  
+  function success() {
+    form.reset();
+    button.style = "display: none ";
+    status.innerHTML = "Thanks! Essar Agro will contact you soon";
+  }
+
+  function error() {
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
